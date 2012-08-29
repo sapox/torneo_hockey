32779e429f933e8f5111b554bb88e3c698bf158c
Juan José Zapponi <zapponi.juan.jose@gmail.com>
2012-08-29 12:20:56 -0300

    sexo del arbitro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120824032839) do
+ActiveRecord::Schema.define(:version => 20120829151306) do
 
   create_table "arbitros", :force => true do |t|
     t.string   "nombre"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20120824032839) do
     t.string   "direccion"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+    t.string   "sexo"
   end
 
   create_table "canchas", :force => true do |t|
