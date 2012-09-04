e18a32edce0dec09bcea1438af20d5d58c336177
Matts <matt987@mathiasen.com>
2012-09-04 09:56:55 -0300

    partidos

--- app/models/partido.rb
+++ app/models/partido.rb
@@ -1,6 +1,8 @@
 class Partido < ActiveRecord::Base
   attr_accessible :cancha_id, :fecha_encuentro
   has_many :partidos_arbitros
+  has_many :equipo_partidos
+  has_many :equipos, through: :equipo_partidos
   belongs_to :cancha
 
 end
