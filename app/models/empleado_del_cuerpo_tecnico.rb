5f7c30468e63ae758ef06263fbd5fbddd08786df
Juan José Zapponi <zapponi.juan.jose@gmail.com>
2012-09-04 12:13:08 -0300

    traduccion errores y que se yo

--- app/models/empleado_del_cuerpo_tecnico.rb
+++ app/models/empleado_del_cuerpo_tecnico.rb
@@ -1,7 +1,7 @@
 class EmpleadoDelCuerpoTecnico < ActiveRecord::Base
   attr_accessible :apellido, :direccion, :dni, :email, :fecha_de_nacimiento, :nombre, :telefono_celular, :telefono_fijo
   has_many :equipos_y_cuerpos_tecnicos
-  validates :apellido, :direccion, :dni, :email, :fecha_de_nacimiento, :nombre, :telefono_celular, :telefono_fijo, :presence => true
+  validates :apellido, :direccion, :dni, :email, :fecha_de_nacimiento, :nombre, :telefono_celular, :telefono_fijo, presence: {:message => "no deberia estar en blanco"}
   validates :nombre, :apellido, :direccion, :format => { :with => /^[a-zA-Z ]+$/, :message => "Solo letras estan permitidas"}
-  validates :dni, :telefono_celular, :telefono_fijo, :numericality => { :only_integer => true }
+  validates :dni, :telefono_celular, :telefono_fijo, :numericality => { :only_integer => true, :message => "Solo numeros permitidos" }
 end
--- app/models/equipo_partido.rb
+++ app/models/equipo_partido.rb
@@ -2,6 +2,6 @@ class EquipoPartido < ActiveRecord::Base
   attr_accessible :cantidad_de_goles, :equipo_id, :partido_id
   belongs_to :equipo
   belongs_to :partido
-  validates :cantidad_de_goles, :equipo_id, :partido_id, :presence => true,
-                                                         :numericality => { :only_integer => true }
+  validates :cantidad_de_goles, :equipo_id, :partido_id, presence: {:message => "no deberia estar en blanco"},
+                                                         :numericality => { :only_integer => true, :message => "Solo numeros permitidos"}
 end
