18d5df1ba978dca6f4b8c8307f45c3c57a68c81a
Juan José Zapponi <zapponi.juan.jose@gmail.com>
2012-09-04 10:52:48 -0300

    canchas

--- app/models/cancha.rb
+++ app/models/cancha.rb
@@ -1,7 +1,7 @@
 class Cancha < ActiveRecord::Base
   attr_accessible :calle_y_numero, :capacidad_publico, :ciudad, :nombre, :pais, :provincia
   has_many :partidos
-  validates :calle_y_numero, :capacidad_publico, :ciudad, :nombre, :pais, :provincia, :presence => true
+  validates :calle_y_numero, :capacidad_publico, :ciudad, :nombre, :pais, :provincia, presence: {:message => "no deberia estar en blanco"}
   validates :ciudad, :nombre, :pais, :provincia, :format => { :with => /^[a-zA-Z ]+$/, :message => "Solo letras estan permitidas"}
-  validates :capacidad_publico, :numericality => { :only_integer => true }, :length => { :minimum => 3 }
+  validates :capacidad_publico, :numericality => { :only_integer => true, message: "solo numeros estan permitidos"}, :length => { :minimum => 3, message: "debe ser mayor a 3 digitos"}
 end
