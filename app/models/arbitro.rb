d1712e2a0f694393611cfe16b03e072877ce5128
Juan José Zapponi <zapponi.juan.jose@gmail.com>
2012-09-04 09:21:37 -0300

    espa para arbitros errors

--- app/models/arbitro.rb
+++ app/models/arbitro.rb
@@ -2,7 +2,7 @@ class Arbitro < ActiveRecord::Base
   attr_accessible :apellido, :direccion, :dni, :email, :sexo, :fecha_de_nacimiento, :nombre, :telefono_celular, :telefono_fijo
   has_many :partidos_arbitros
   validates :apellido, :direccion, :dni, :email, :sexo, :fecha_de_nacimiento, :nombre, :telefono_celular, :telefono_fijo,
-  :presence => true
+  presence: {:message => "No deberia estar en blanco"}
   validates :nombre, :apellido, :format => { :with => /^[a-zA-Z ]+$/, :message => "Solo letras estan permitidas"}
-  validates :dni, :telefono_celular, :telefono_fijo, :numericality => { :only_integer => true }
+  validates :dni, :telefono_celular, :telefono_fijo, :numericality => { :only_integer => true , :message => "Solo numeros estan permitidos"}
 end
